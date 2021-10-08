7d102e744376cbe8b469eb38ba4bab08988b4659
Kasper <kasper.gaworski@wp.pl>
2021-10-08 16:26:06 +0200

    charity

--- tabs/Charity.js
+++ tabs/Charity.js
@@ -11,9 +11,12 @@ export default function Charity() {
       <Text style={styles.text}>Zbiórki charytatywne</Text>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.charityCardContainer}>
-          <CharityCard imageUrl='https://zrzutka.pl/uploads/chipin/xthfdf/cover/orginal/c2a5c5b24691dcefa5f360d88593bd44.jpeg' title='Zalane schronisko w Wadowicach Dolnych' link='https://google.com/'/>
-          <CharityCard imageUrl='https://zrzutka.pl/uploads/chipin/25c6ud/cover/orginal/3bc50bbeaae246106f778afcff08b400.jpeg' title='Ratujmy życie Konrada, który sam nie bał się go ofiarować nam wszystkim.' link='https://zrzutka.pl/25c6ud'/>
+          <CharityCard imageUrl='https://zrzutka.pl/uploads/chipin/dm2c7n/cover/orginal/141cf69d6f2fbcfd0763d7badd4d385d.jpeg' title='Wspólnie ocalmy nasz las !!!' link='https://zrzutka.pl/wspolnylas'/>
+          <CharityCard imageUrl='https://www.lasnazawsze.org.pl/wp-content/uploads/sites/52/2020/04/beautiful-beauty-color-dawn-340932-1024x543.jpg' title='Las na zawsze - posadz kawałek lasu' link='https://www.lasnazawsze.org.pl/'/>
           <CharityCard imageUrl='https://zrzutka.pl/uploads/chipin/wggzr7/cover/orginal/51c6ba3dec3d05b3c71bb2352d2b5c45.jpeg' title='300 zwierząt bez jedzenia i leczenia' link='https://zrzutka.pl/wggzr7'/>
+          <CharityCard imageUrl='https://cdn.shopify.com/s/files/1/0326/7189/products/Spain-landscape_2000x.png?v=1602174676' title='One tree planted - WE PLANT TREES TOGETHER, PLANTING A FOREST.' link='https://onetreeplanted.org/'/>
+          <CharityCard imageUrl='https://internationaltreefoundation.org/wp-content/uploads/2020/06/NKA-BOLGA022-e1627052010350-1024x409.jpg' title='International Tree Fundation' link='https://internationaltreefoundation.org/'/>
+
         </View>
       </ScrollView>
     </View>
